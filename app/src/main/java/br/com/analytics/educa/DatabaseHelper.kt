d5d8e74e2159a8ad78ed6848f0143f7be018203b
Your Name <thiago.bafi@outlook.com>
2024-11-21 23:26:48 -0300

    a

---
+++ app/src/main/java/br/com/analytics/educa/DatabaseHelper.kt
@@ -0,0 +1,46 @@
+package br.com.analytics.educa
+
+import android.content.Context
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.SQLException
+
+class DatabaseHelper(private val context: Context) {
+
+    private val connectionUrl =
+        "jdbc:sqlserver://localhost:1433;databaseName=EducaAnalytics;user=Thiago;password=1709"
+
+    fun connect(): Connection? {
+        var connection: Connection? = null
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+            connection = DriverManager.getConnection(connectionUrl)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+        }
+        return connection
+    }
+
+    fun executeQuery(query: String): ResultSet? {
+        var resultSet: ResultSet? = null
+        try {
+            val connection = connect()
+            val statement = connection?.createStatement()
+            resultSet = statement?.executeQuery(query)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+        return resultSet
+    }
+
+    fun closeConnection(connection: Connection?) {
+        try {
+            connection?.close()
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+    }
+}
