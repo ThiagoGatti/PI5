6c7e7d1507a6e465a8776da9ecb0395dd3d7bba3
Your Name <thiago.bafi@outlook.com>
2024-11-27 01:31:01 -0300

    Adicionar Novas Rotas

--- app/src/main/java/br/com/analytics/educa/ui/route/Route.kt
+++ app/src/main/java/br/com/analytics/educa/ui/route/Route.kt
@@ -6,8 +6,15 @@ object Route {
     const val login = "login" // tela login
     const val userVerification = "userVerification" // verificacao usuario
     const val menuAluno = "menuAluno" // tela menu aluno
-    const val menuFormAluno = "menuFormEducando"
-    const val answerFormAlunoAutonomia = "answerFormEducandosAutonomia" // tela responder formulario 1 educando
+    const val menuFormAluno = "menuFormAluno"
+        const val FormAlunoAutonomia = "FormAlunoAutonomia" // tela responder formulario 1 educando
+        const val formAlunoAutonomia = "formAlunoAutonomia"
+        const val formAlunoClimaEscolar = "formAlunoClimaEscolar"
+        const val formAlunoQualidadeEnsino = "formAlunoQualidadeEnsino"
+        const val formAlunoInfraestrutura = "formAlunoInfraestrutura"
+        const val formAlunoGestao = "formAlunoGestao"
+
+
     const val screenFour = "screenFour"
     const val screenFive = "screenFive"
 
