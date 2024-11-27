d0047e6f15cefe5e340b8c844e01d40b48c32ee8
Your Name <thiago.bafi@outlook.com>
2024-11-27 01:08:54 -0300

    add route

--- app/src/main/java/br/com/analytics/educa/ui/route/Route.kt
+++ app/src/main/java/br/com/analytics/educa/ui/route/Route.kt
@@ -1,9 +1,14 @@
 package br.com.analytics.educa.ui.route
 
 object Route {
-    const val screenOne = "screenOne"
-    const val screenTwo = "screenTwo"
-    const val screenThree = "screenThree"
+
+    const val initialScreen = "initialScreen" // tela inicial
+    const val login = "login" // tela login
+    const val userVerification = "userVerification" // verificacao usuario
+    const val menuAluno = "menuAluno" // tela menu aluno
+    const val menuFormAluno = "menuFormEducando"
+    const val answerFormAlunoAutonomia = "answerFormEducandosAutonomia" // tela responder formulario 1 educando
     const val screenFour = "screenFour"
     const val screenFive = "screenFive"
+
 }
