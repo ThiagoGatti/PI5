8a81062779822306384339600fea092fcbcdda17
Your Name <thiago.bafi@outlook.com>
2024-11-27 02:37:21 -0300

    Adicionar Novas Rotas

--- app/src/main/java/br/com/analytics/educa/ui/route/Route.kt
+++ app/src/main/java/br/com/analytics/educa/ui/route/Route.kt
@@ -2,20 +2,42 @@ package br.com.analytics.educa.ui.route
 
 object Route {
 
+    // TELAS INICIAIS
     const val initialScreen = "initialScreen" // tela inicial
     const val login = "login" // tela login
     const val userVerification = "userVerification" // verificacao usuario
+
+    // TELAS ALUNOS
+
     const val menuAluno = "menuAluno" // tela menu aluno
-    const val menuFormAluno = "menuFormAluno"
-        const val FormAlunoAutonomia = "FormAlunoAutonomia" // tela responder formulario 1 educando
-        const val formAlunoAutonomia = "formAlunoAutonomia"
-        const val formAlunoClimaEscolar = "formAlunoClimaEscolar"
-        const val formAlunoQualidadeEnsino = "formAlunoQualidadeEnsino"
-        const val formAlunoInfraestrutura = "formAlunoInfraestrutura"
-        const val formAlunoGestao = "formAlunoGestao"
+        const val menuAlunoForm = "menuAlunoForm"
+            const val formAlunoAutonomia = "formAlunoAutonomia"
+            const val formAlunoClimaEscolar = "formAlunoClima"
+            const val formAlunoQualidadeEnsino = "formAlunoQualidade"
+            const val formAlunoInfraestrutura = "formAlunoInfraestrutura"
+            const val formAlunoGestao = "formAlunoGestao"
 
+    // TELAS PROFESSORES
 
-    const val screenFour = "screenFour"
-    const val screenFive = "screenFive"
+    const val menuProfessores = "menuProfessores"
+        const val menuProfessoresForm = "menuProfessoresForm"
+            const val formProfessoresClima = "formProfessoresClima"
+            const val formProfessoresQualidade = "formProfessoresQualidade"
+            const val formProfessoresCondicao = "formProfessoresCondicao"
+            const val formProfessoresParticipacao = "formProfessoresParticipacao"
+            const val formProfessoresDesafios = "formProfessoresDesafios"
+
+    // TELAS FUNCIONARIOS
+
+    const val menuFuncionarios = "menuFuncionarios"
+        const val menuFuncionariosForm = "menuFuncionariosForm"
+            const val formFuncionariosDesafios = "formFuncionariosDesafios"
+            const val formFuncionariosEficiencia = "formFuncionariosEficiencia"
+            const val formFuncionariosInfraestrutura = "formFuncionariosInfraestrutura"
+            const val formFuncionariosSatisfacao = "formFuncionariosSatisfacao"
+
+    // TELAS DIRETORES
+
+    const val menuDiretores = "menuDiretores"
 
 }
