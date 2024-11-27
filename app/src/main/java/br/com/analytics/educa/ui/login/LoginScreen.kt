c4874de222cb656a10abd4f1d269f07a9ea54e76
Your Name <thiago.bafi@outlook.com>
2024-11-27 02:47:07 -0300

    Update LoginScreen.kt

--- app/src/main/java/br/com/analytics/educa/ui/login/LoginScreen.kt
+++ app/src/main/java/br/com/analytics/educa/ui/login/LoginScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(
     onLoginSuccess: () -> Unit,
