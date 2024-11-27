6e46023a25974dca9c7a9d5b16247f44d3f27cb7
Your Name <thiago.bafi@outlook.com>
2024-11-27 02:25:11 -0300

    Arrumar MenuAlunos

---
+++ app/src/main/java/br/com/analytics/educa/ui/menu/MenuAlunos.kt
@@ -0,0 +1,27 @@
+package br.com.analytics.educa.ui.menu
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MenuAluno (
+    navigateToMenuFormAluno: () -> Unit,
+    navigateBack: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Menu")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = navigateToMenuFormAluno) { Text("Formulários Avaliativos") }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = navigateBack) { Text("Voltar") }
+    }
+}
