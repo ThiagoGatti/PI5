69d13ba8d63e7b56ea5232083f5311df027fd724
Your Name <thiago.bafi@outlook.com>
2024-11-27 17:31:03 -0300

    Update Route.kt

--- app/src/main/java/br/com/analytics/educa/ui/route/Route.kt
+++ app/src/main/java/br/com/analytics/educa/ui/route/Route.kt
@@ -16,7 +16,6 @@ object Route {
             const val formAlunoQualidadeEnsino = "formAlunoQualidade"
             const val formAlunoInfraestrutura = "formAlunoInfraestrutura"
             const val formAlunoGestao = "formAlunoGestao"
-            const val formAlunoConsideracoesFinais = "formAlunoConsideracoes"
 
     // TELAS PROFESSORES
 
