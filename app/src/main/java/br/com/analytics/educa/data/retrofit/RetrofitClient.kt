d06be9a05e94438856e2ca3ab2bf052231d2f53f
Vinícius Scott <viniciusfscott@gmail.com>
2024-12-06 11:47:50 -0300

    banco local emulador.

--- app/src/main/java/br/com/analytics/educa/data/retrofit/RetrofitClient.kt
+++ app/src/main/java/br/com/analytics/educa/data/retrofit/RetrofitClient.kt
@@ -4,11 +4,18 @@ import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.Inet4Address
+import java.net.NetworkInterface
+
+val BASE_URL = if (getLocalIpAddress().startsWith("10.0.2")) {
+    "http://10.0.2.2/"
+} else {
+    "http://educaserver.ddns.net/"
+}
 
 class RetrofitClient {
     companion object {
         private lateinit var INSTANCE: Retrofit
-        private const val BASE_URL = "http://educaserver.ddns.net/"
         private fun getRetrofitInstance(): Retrofit {
             val httpClient = OkHttpClient.Builder()
                 .retryOnConnectionFailure(true)
@@ -28,4 +35,24 @@ class RetrofitClient {
             return getRetrofitInstance().create(serviceClass)
         }
     }
+}
+
+fun getLocalIpAddress(): String {
+    try {
+        val interfaces = NetworkInterface.getNetworkInterfaces()
+        while (interfaces.hasMoreElements()) {
+            val inetAddresses = interfaces.nextElement().inetAddresses
+            while (inetAddresses.hasMoreElements()) {
+                val inetAddress = inetAddresses.nextElement()
+                if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) {
+                    println("teste ${inetAddress.hostAddress}")
+                    return inetAddress.hostAddress ?: ""
+                }
+            }
+        }
+    } catch (e: Exception) {
+        println("teste")
+        e.printStackTrace()
+    }
+    return ""
 }
